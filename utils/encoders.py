cd9ac22de1e3ce655b8ab177ceef5d5e9fb56b0d
borisoid <boris.lazarev90@mail.ru>
2022-04-05 22:26:16 +0300

    Fix typing

--- utils/encoders.py
+++ utils/encoders.py
@@ -1,18 +1,24 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from string import ascii_uppercase
-from typing import TypeVar, Sequence, Generic
+from typing import Generic, Optional, Sequence, TypeVar
 
 
 unencoded_data = TypeVar('unencoded_data')
-encoded_data = TypeVar('encoded_data', bound=Sequence[int])
+encoded_data = TypeVar('encoded_data', bound=Sequence[Optional[int]])
 
 
 class Encoder(ABC, Generic[unencoded_data]):
+    @abstractmethod
     def encode(self, data: unencoded_data) -> encoded_data:
         pass
 
+    @abstractmethod
     def decode(self, data: encoded_data) -> unencoded_data:
         pass
+
+    def _assert_none_not_in(self, data: encoded_data) -> None:
+        if None in data:
+            raise ValueError('Encoded data contains None.')
 
 
 class TextEncoder(Encoder[str]):
@@ -29,8 +35,9 @@ class TextEncoder(Encoder[str]):
                 ind_list.append(None)
         return tuple(ind_list)
 
-    def decode(self, text: encoded_data) -> str:
-        return ''.join(map(lambda i: self.alphabet[i], text))
+    def decode(self, data: encoded_data) -> str:
+        self._assert_none_not_in(data)
+        return ''.join(map(lambda i: self.alphabet[i], data))
 
 
 ascii_uppercase_encoder = TextEncoder(ascii_uppercase)
